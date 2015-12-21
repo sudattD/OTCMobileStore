ccebb82812d48957eef08553eac615103f492cea
davesuda <sudatt.dave@gmail.com>
2015-12-21 19:24:21 +0530

    save 21 dec adding tables and more

--- app/src/main/java/com/otc/utility/SqLiteHelper.java
+++ app/src/main/java/com/otc/utility/SqLiteHelper.java
@@ -54,17 +54,47 @@ public class SqLiteHelper extends SQLiteOpenHelper {
 
 
     private static final String TABLE_CATEGORY_DETAIL = "CategoryDetails";
-    private static final String CATEGORY_ID = "categoryID";
-    private static final String PARENT_ID = "parentID";
+    private static final String CATEGORY_ID = "categoryId";
+    private static final String PARENT_ID = "parentId";
     private static final String NAME = "name";
     private static final String CREATED_AT = "CreatedAt";
     private static final String UPDATED_AT = "UpdatedAt";
 
-    //##########################################################################################/////
+//##########################################################################################/////
 
 
+    private static final String TABLE_PRODUCT_DETAIL = "ProductDetails";
+    private static final String PRO_CATEGORY_ID = "categoryId";
+    private static final String PRO_NAME = "productName";
+    private static final String PRO_DESCRIPTION = "productdesc";
+    private static final String PRO_SKU = "prosku";
+    private static final String PRO_PRICE = "proPrice";
+    private static final String PRO_TAXABLE = "isTaxable";
+    private static final String PRO_QUANTITY = "quantity";
+    private static final String PRO_CREATED_AT = "CreatedAt";
+    private static final String PRO_UPDATED_AT = "UpdatedAt";
 
 
+//##########################################################################################/////
+
+
+    private static final String TABLE_SETTINGS = "SettingsDetails";
+    private static final String SETTINGS_ID = "SettingsId";
+    private static final String LAST_SYNC = "LastSync";
+    private static final String TAG_LINE = "TagLine";
+    private static final String FOOTER_TEXT = "FooterText";
+    private static final String SETTINGS_UPDATED_DATETIME = "updatedDateTime";
+
+
+//##########################################################################################/////
+
+    private static final String TABLE_LOGS = "LogsTableDetails";
+    private static final String LOG_DATA = "LogData";
+    private static final String LOG_DETAIL = "LogDetails";
+    private static final String LOGS_DATETIME = "LogsDateTime";
+
+
+//##########################################################################################/////
 
 
 
@@ -86,6 +116,30 @@ public class SqLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+
+        String CREATE_CUSTOMER_DETAILS_TABLE = "CREATE TABLE "
+                + TABLE_CUSTOMER_DETAILS + "("
+                + CUSTOMER_COLUMN_ID + " INTEGER PRIMARY KEY,"
+                + CUSTOMER_FIRST_NAME + " TEXT,"
+                + CUSTOMER_LAST_NAME + " TEXT,"
+                + CUSTOMER_OTC_CARD + " TEXT,"
+                + CUSTOMER_EMAIL + " TEXT,"
+                + CUSTOMER_MOBILE + " TEXT,"
+                + CUSTOMER_STREET + " TEXT,"
+                + CUSTOMER_CITY + " TEXT,"
+                + CUSTOMER_STATE + " TEXT,"
+                + CUSTOMER_POSTAL_CODE + " TEXT,"
+                + CUSTOMER_COUNTRY_ID + " TEXT,"
+                + CUSTOMER_CREATED_AT + " TEXT,"
+                + CUSTOMER_UPDATED_AT + " TEXT"
+                + ")";
+
+
+        db.execSQL(CREATE_CUSTOMER_DETAILS_TABLE);
+
+
+
 
     }
 
